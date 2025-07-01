cfcc05588a7efb1b55f66d6c0b187392f6f75796
mrckie <marckie.js@gmail.com>
2025-07-01 18:25:56 +0800

    initial commit

---
+++ test.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import joblib
+
+
+model = joblib.load('spam_classifier.pkl')
+vertorizer = joblib.load('vectorizer.pkl')
+
+st.title('SMS Spam Classifier')
+st.write("Enter a message and let the model predict if it's **Spam** or **Ham**")
+
+user_input = st.text_area('Type your message here: ')
+
+if st.button("Classify"):
+    if user_input.strip() == "":
+        st.warning("Please enter a message")
+    else:
+        vectorized_input = vertorizer.transform(([user_input]))
+        prediction = model.predict(vectorized_input)[0]
+        prediction_prob = model.predict_probs(vectorized_input).max()
+
+        if prediction == 'spam':
+            st.error(f"Prediction: **Spam** ({prediction_prob:.2%} confidence)" )
+        else: 
+            st.success(f"Prediction: **Ham** ({prediction_prob:.2%} confidence)" )
